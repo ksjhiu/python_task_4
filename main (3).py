08f13fbbfa0dfd57cece89952df84e229c92c192
ksjhiu <94771016+ksjhiu@users.noreply.github.com>
2022-03-31 10:54:57 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,20 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+E =[8,0,6,2,4];
+N =[1,3,5,2,4];
+print("Union of E and N is ");
+print(set(E).union(N));
+print("Intersection of E and N is ");
+print(set(E).intersection(N));
+print("Difference of E and N is ");
+print(set(E)-set(N));
+print("Symmetric difference of E and N is");
+print(set(E).symmetric_difference(N));
+
+
